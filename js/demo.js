becdaf8c784a96c4536702cfe59aa2161a022373
Monica <monica.rolle13@gmail.com>
2021-02-08 13:47:31 +0100

    img change and button push

--- js/demo.js
+++ js/demo.js
@@ -1,3 +1,10 @@
 document.getElementById('name').value = "Mi nombre";
 document.getElementById('lastName').value = "Mi apellido";
 
+document.getElementById('avatar').src = "img/profile.png"
+
+
+function getData(){
+    console.log(document.getElementById('name').value);
+    console.log(document.getElementById('lastName').value);
+}
